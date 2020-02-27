feba58331a6a1973b1636df60494adf47a948d13
John Kilbride <jkilbride@gmail.com>
2020-02-27 11:57:05 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  
+  validates :username, presence: true,
+                        uniqueness: {case_sensitve: false},
+                        length: {minimum: 3, maximum: 25}
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                        uniqueness: {case_sensitve: false},
+                        length: {minimum: 3, maximum: 105},
+                        format: {with: VALID_EMAIL_REGEX}
+
+  # user is present and unique
+  # email is present and unique
+  # valid email address
 end
