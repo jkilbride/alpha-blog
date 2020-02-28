d5ac4201b754c565390e2a0826820b22e3561ef4
John Kilbride <jkilbride@gmail.com>
2020-02-28 16:24:13 +0000

    Protected sensitive actions behind authentication system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :require_same_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -18,17 +21,14 @@ class UsersController < ApplicationController
   end
 
   def show
-      @user = User.find(params[:id])
       @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def edit
-      @user = User.find(params[:id])
 
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was successfully updated"
       redirect_to articles_path
@@ -40,5 +40,16 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if logged_in? && current_user != @user
+        flash[:danger] = 'You can only edit your own profile'
+        redirect_to root_path
+      end
     end
 end
